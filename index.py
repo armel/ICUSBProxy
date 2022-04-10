7cf74608ae6a9059cef593505e4df1e04a0c88fa
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-10 05:21:55 +0200

    First test

--- index.py
+++ index.py
@@ -28,7 +28,18 @@ try:
     usb.close()
 
 except:
-    pass
+    baudrate = 9600
+    serialport = "/dev/ttyUSB2"
+    usb = serial.Serial(serialport, baudrate)
+    usb.setDTR(False)
+    usb.setRTS(False)
+
+    civ = ["0xfe","0xfe","0xA4","0xe0","0x00","0x56","0x34","0x12","0x07","0x00","0xfd"]
+
+    for value in civ:
+        print(value)
+        usb.write(struct.pack('>B', int(bytes(value), 'UTF-8'), 16))
+    usb.close()
 
 # End properly
 
