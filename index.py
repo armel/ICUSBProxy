46b113635134c236be9bff60c11a2e044f45c69d
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-10 05:23:05 +0200

    First test

--- index.py
+++ index.py
@@ -10,6 +10,7 @@ from time import sleep
 form = cgi.FieldStorage()
 print("Content-type: text/html; charset=utf-8\n")
 
+'''
 try:
     arg = cgi.FieldStorage()
     civ = arg['civ'].value
@@ -28,18 +29,21 @@ try:
     usb.close()
 
 except:
-    baudrate = 9600
-    serialport = "/dev/ttyUSB2"
-    usb = serial.Serial(serialport, baudrate)
-    usb.setDTR(False)
-    usb.setRTS(False)
+    pass
+'''
+    
+baudrate = 9600
+serialport = "/dev/ttyUSB2"
+usb = serial.Serial(serialport, baudrate)
+usb.setDTR(False)
+usb.setRTS(False)
 
-    civ = ["0xfe","0xfe","0xA4","0xe0","0x00","0x56","0x34","0x12","0x07","0x00","0xfd"]
+civ = ["0xfe","0xfe","0xA4","0xe0","0x00","0x56","0x34","0x12","0x07","0x00","0xfd"]
 
-    for value in civ:
-        print(value)
-        usb.write(struct.pack('>B', int(bytes(value), 'UTF-8'), 16))
-    usb.close()
+for value in civ:
+    print(value)
+    usb.write(struct.pack('>B', int(bytes(value), 'UTF-8'), 16))
+usb.close()
 
 # End properly
 
