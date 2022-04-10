05b481e7aafcac610207bc7ef9eef6c6ad025343
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-10 05:15:01 +0200

    First test

---
+++ index.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import cgi 
+import time
+import serial
+import struct
+from time import sleep
+
+form = cgi.FieldStorage()
+print("Content-type: text/html; charset=utf-8\n")
+
+try:
+    arg = cgi.FieldStorage()
+    civ = arg['civ'].value
+
+    baudrate = 9600
+    serialport = "/dev/ttyUSB2"
+    usb = serial.Serial(serialport, baudrate)
+    #sleep(0.5)
+    usb.setDTR(False)
+    usb.setRTS(False)
+
+    civ = ["0xfe","0xfe","0xA4","0xe0","0x00","0x56","0x34","0x12","0x07","0x00","0xfd"]
+
+    for value on civ:
+        usb.write(struct.pack('>B', int(bytes(value), 'UTF-8'), 16)))
+    usb.close()
+
+except:
+    pass
+
+# End properly
+
+exit()
---
+++ server.py
@@ -0,0 +1,12 @@
+import http.server
+ 
+PORT = 1234
+server_address = ("", PORT)
+
+server = http.server.HTTPServer
+handler = http.server.CGIHTTPRequestHandler
+handler.cgi_directories = ["/"]
+print("Serveur actif sur le port :", PORT)
+
+httpd = server(server_address, handler)
+httpd.serve_forever()
