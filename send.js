ffa391037ca5021446f05a514aa6e6ed912e186b
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-10 04:10:32 +0200

    First test

--- send.js
+++ send.js
@@ -1,15 +1,33 @@
-var SerialPort = require('serialport').SerialPort;
+var buffer = new Buffer(7);
+buffer[0] = 0xFE;
+buffer[1] = 0xFE;
+buffer[2] = 0xA4;
+buffer[3] = 0xE0;
+buffer[4] = 0x15;
+buffer[5] = 0x15;
+buffer[6] = 0xFD;
+
 var port = "/dev/ttyUSB2";
-var commande = "FEFEA4E01515FD";
-const path = require('path'); 
 
-var serialPort = new SerialPort(port, {
-  baudRate: 9600
-});
 
-serialPort.write(Buffer.from(commande, "hex"), function(err) {
-  if (err) {
-    return console.log("Error on write: ", err.message);
-  }
-  console.log("Message sent successfully");
+var com = new SerialPort(port, {
+    baudRate: 9600,
+    databits: 8,
+    parity: 'none'
+}, false);
+
+com.open(function (error) {
+    if (error) {
+        console.log('Error while opening the port ' + error);
+    } else {
+        console.log('CST port open');
+        com.write(buffer, function (err, result) {
+            if (err) {
+                console.log('Error while sending message : ' + err);
+            }
+            if (result) {
+                console.log('Response received after sending message : ' + result);
+            }    
+        });
+    }              
 });
