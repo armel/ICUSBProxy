f9e7c715fc9da4898b461419ad9a026d59f25fee
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-10 05:20:20 +0200

    First test

--- index.py
+++ index.py
@@ -17,13 +17,13 @@ try:
     baudrate = 9600
     serialport = "/dev/ttyUSB2"
     usb = serial.Serial(serialport, baudrate)
-    #sleep(0.5)
     usb.setDTR(False)
     usb.setRTS(False)
 
     civ = ["0xfe","0xfe","0xA4","0xe0","0x00","0x56","0x34","0x12","0x07","0x00","0xfd"]
 
     for value in civ:
+        print(value)
         usb.write(struct.pack('>B', int(bytes(value), 'UTF-8'), 16))
     usb.close()
 
