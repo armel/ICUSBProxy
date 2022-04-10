02358d98d5f3339d223f34b99f646b6c1dc3fa17
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-10 03:23:59 +0200

    First test

--- send.js
+++ send.js
@@ -1,23 +1,14 @@
 var SerialPort = require("serialport");
 var port = "/dev/USB2";
-var commande = {0xFE, 0xFE, 0xA4, 0xE0, 0x15, 0x15, 0xFD};
+var commande = "FEFEA4E01515FD";
 
 var serialPort = new SerialPort(port, {
   baudRate: 9600
 });
 
-serialPort.write(hex(commande), function(err) {
+serialPort.write(Buffer.from(commande, "hex"), function(err) {
   if (err) {
     return console.log("Error on write: ", err.message);
   }
   console.log("Message sent successfully");
 });
-
-function hex(str) {
-  var arr = [];
-  for (var i = 0, l = str.length; i < l; i ++) {
-          var ascii = str.charCodeAt(i);
-          arr.push(ascii);
-  }
-  return new Buffer(arr);
-}
