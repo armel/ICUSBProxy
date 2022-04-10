40ec90ba1842482e8edcd940edaa4aaa6ddd9bb8
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-10 03:32:25 +0200

    First test

--- send.js
+++ send.js
@@ -1,6 +1,7 @@
 var SerialPort = require('serialport').SerialPort;
 var port = "/dev/ttyUSB2";
 var commande = "FEFEA4E01515FD";
+const path = require('path'); 
 
 var serialPort = new SerialPort(port, {
   baudRate: 9600
