f5d83df71e107bbc827521ab1a3c513d2fbf6f4c
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-10 23:16:14 +0200

    First test

---
+++ proxy.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Copyright (c) F4HWN Armel. All rights reserved.
+Licensed under the MIT license. See LICENSE file in the project root for full license information.
+'''
+
+import settings as s
+import cgi 
+import time
+import serial
+import struct
+from time import sleep
+
+form = cgi.FieldStorage()
+print('Content-type: text/html; charset=utf-8\n')
+
+try:
+    arg = cgi.FieldStorage()
+    civ = arg['civ'].value
+except:
+    civ = ','
+
+usb = serial.Serial(s.client_serial, s.client_baudrate, timeout=0.01)
+usb.setDTR(False)
+usb.setRTS(False)
+
+#civ = 'fe,fe,A4,e0,00,56,34,12,07,00,fd,'  # Debug trace
+
+civ = civ[:-1]
+civ = civ.split(",")
+
+for value in civ:
+    value = '0x' + value
+    data = int(bytes(value).encode('utf-8'), 16)
+    usb.write(struct.pack('>B', data))
+
+response = ''
+data = usb.read(size=16) #Set size to something high
+for value in data:
+    response += value.encode('hex')
+
+print(response)
+
+usb.close()
+
+# End properly
+
+exit()
---
+++ settings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Copyright (c) F4HWN Armel. All rights reserved.
+Licensed under the MIT license. See LICENSE file in the project root for full license information.
+'''
+
+# Version
+version = '0.0.1'
+
+# Server settings
+server_port = 1234
+
+# Proxy settings
+client_serial = '/dev/ttyUSB2'
+client_baudrate = 115900
