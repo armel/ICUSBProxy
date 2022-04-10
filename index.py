e6005aeb37167c71a2b5cdbd072e7b9910a81c83
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-10 06:07:00 +0200

    First test

--- index.py
+++ index.py
@@ -34,7 +34,7 @@ print('-----')
 byteData = usb.read(size=16) #Set size to something high
 print(len(byteData))
 for value in byteData:
-    print(hex(value))
+    print(b.value.hex())
 
 usb.close()
 
