3a7c8c5293dd4fddf73b49199801f5b5b5932d77
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-11 00:13:35 +0200

    First test

--- proxy.py
+++ proxy.py
@@ -14,11 +14,13 @@ import serial
 form = cgi.FieldStorage()
 print('Content-type: text/html; charset=utf-8\n')
 
+'''
 try:
     arg = cgi.FieldStorage()
     civ = arg['civ'].value
 except:
     civ = ','
+'''
 
 usb = serial.Serial(s.client_serial, s.client_baudrate, timeout=0.2)
 usb.setDTR(False)
