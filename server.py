78c35daf7a4c5cacd18016a17a2fb2908c0b2822
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-11 02:00:19 +0200

    First test

--- server.py
+++ server.py
@@ -53,7 +53,7 @@ class S(BaseHTTPRequestHandler):
         # End properly
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
-        self.send(response)
+        self.wfile.write(response)
         self.end_headers()
 
 
