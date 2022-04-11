60335fe6e2a5f76b03cb4375f420ea6f6fd3a911
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-11 02:04:49 +0200

    First test

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ class S(BaseHTTPRequestHandler):
 
     def do_GET(self):
         #logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        #self._set_response()
+        self._set_response()
         #self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
 
         civ = str(self.path).split('=')
