6e2a9558eb94c658dcae5a8b19294133b5ae6b98
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-14 03:03:42 +0200

    Test

--- icom_civ_provy.py
+++ icom_civ_provy.py
@@ -16,11 +16,15 @@ class S(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
+        self.send_header('Cache-Control', 'no-store, must-revalidate')
+        self.send_header('Expires', '0')
         self.end_headers()
 
     def _set_error(self):
         self.send_response(404)
         self.send_header('Content-type', 'text/html')
+        self.send_header('Cache-Control', 'no-store, must-revalidate')
+        self.send_header('Expires', '0')
         self.end_headers()
 
     def do_GET(self):
