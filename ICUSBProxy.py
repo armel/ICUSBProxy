7dc6c4fe874047877b3bfb2d669bfb55b55fb776
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-20 18:58:57 +0200

    Improve code

--- ICUSBProxy.py
+++ ICUSBProxy.py
@@ -96,9 +96,10 @@ if __name__ == '__main__':
     from sys import argv
 
     if len(argv) == 2:
+        verbose = 0
         run(port=int(argv[1]))
     elif len(argv) == 3:
-        verbose = int(argv[2]))
+        verbose = int(argv[2])
         run(port=int(argv[1]))
     else:
         run()
