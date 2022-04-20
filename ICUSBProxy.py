28d3c86c341b521c2c9da5cac1a9a75d58004ac8
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-20 19:00:32 +0200

    Improve code

--- ICUSBProxy.py
+++ ICUSBProxy.py
@@ -13,6 +13,7 @@ import serial
 name = "ICUSBProxy"
 version = "0.0.3"
 client_timeout = 0.02
+server_verbose = 0
 
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
@@ -26,7 +27,7 @@ class S(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        if verbose > 1:        
+        if server_verbose > 1:        
             logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
 
         civ = str(self.path).split('=')
@@ -65,7 +66,7 @@ class S(BaseHTTPRequestHandler):
             if(response == "fefee0" + client_adresse + "fafd"):
                 response = ''
         except:
-            if verbose > 0:
+            if server_verbose > 0:
                 print('Check if serial device ' + client_serial + ' at ' + client_baudrate + ' is up...')
             self._set_error()
 
@@ -80,7 +81,7 @@ class S(BaseHTTPRequestHandler):
         return
 
 def run(server_class=HTTPServer, handler_class=S, port=1234):
-    if verbose > 1:
+    if server_verbose > 1:
         logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
@@ -96,10 +97,10 @@ if __name__ == '__main__':
     from sys import argv
 
     if len(argv) == 2:
-        verbose = 0
+        server_verbose = 0
         run(port=int(argv[1]))
     elif len(argv) == 3:
-        verbose = int(argv[2])
+        server_verbose = int(argv[2])
         run(port=int(argv[1]))
     else:
         run()
