c0346bde4fc2eb383f2d39f53204e21c9a26f3f0
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-05-03 19:19:25 +0200

    Improve

--- ICUSBProxy.py
+++ ICUSBProxy.py
@@ -50,7 +50,7 @@ class S(BaseHTTPRequestHandler):
 
             try:
                 #usb = serial.Serial(client_serial, client_baudrate, timeout=client_timeout)
-                usb = serial.Serial(client_serial, client_baudrate, timeout=None)
+                usb = serial.Serial(client_serial, client_baudrate, timeout=0)
                 
                 # Send command
                 command = []
