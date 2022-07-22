b2f067b529a598cf67f1dc375d2defe08f565fe1
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-07-22 06:02:39 +0200

    Enum

--- ICUSBProxy.py
+++ ICUSBProxy.py
@@ -119,7 +119,7 @@ class S(BaseHTTPRequestHandler):
                         print(response)
 
                         # Check if bad response    
-                        if(response == "fefee0" + client_address + "fafd" || response == ''):
+                        if(response == "fefee0" + client_address + "fafd" or response == ''):
                             response = ''
                             break
                         else:
